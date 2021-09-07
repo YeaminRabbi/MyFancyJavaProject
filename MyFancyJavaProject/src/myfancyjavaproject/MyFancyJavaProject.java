403d0565821f790fdb2fffe8c202095952b41b99
JariaTanha <jtanha171256@bscse.uiu.ac.bd>
2021-09-07 22:59:41 +0600

    Singleton code implemented

--- MyFancyJavaProject/src/myfancyjavaproject/MyFancyJavaProject.java
+++ MyFancyJavaProject/src/myfancyjavaproject/MyFancyJavaProject.java
@@ -15,19 +15,38 @@ public class MyFancyJavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Car object = new Car("Toyota", 100000);
-        
-        System.out.println("Car Name: "+ object.name + " | Car price: "+ object.price);
+        Car x = Car.getInstance();
+	Car y = Car.getInstance();
+
+
+	System.out.println("Hashcode of x is "+ x.hashCode());
+        System.out.println("Hashcode of y is "+ y.hashCode());
+
+	if (x == y) {
+            System.out.println("Two objects point shares the same memory location");
+        }
+ 
+        else {
+           
+            System.out.println("Two objects shares different memory location");
+        }
     }
     
 }
 
-
 class Car{
-    String name;
-    int price;
-    Car(String name, int price){
-        this.name=name;
-        this.price = price;
+    private static Car single_instance = null;
+    public String str;
+    private Car()
+    {
+        str = "This is a Singleton class";
+    }
+
+    public static Car getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Car();
+ 
+        return single_instance;
     }
 }
